f86e6a30b1f7c91574c8adf99e092a2ff6ddab6b
Devindra Payment <Arden.ived@gmail.com>
2013-03-13 10:47:03 -0700

    Code Formatting

--- js/evaluate-admin.js
+++ js/evaluate-admin.js
@@ -1,153 +1,160 @@
 var Evaluate_Admin = {
-  ////the two global variables, required to display
-  //a preview of the metric before form submission
-  type : new String,
-  style : new String,
-
-  /*
-   *adds a new poll question answer in the metric form
-   */
-  addNewAnswer : function() {
-    var answers = jQuery('input[name*="evalu_form[poll][answer]"]'); //fetch all active answer inputs
-    var num = answers.length + 1; //determine answer number
-    var last_field = answers.splice(-1,1); // get last element
-    //insert new answer field after the current last one
-    //since we select by input name, the parent of parent i.e. <div class="indent"> will be the element
-    //which we add the last field after.
-    jQuery(last_field).parent().parent().after(
-      '<div class="indent"><label>Answer ' + num + ': <input type="text" name="evalu_form[poll][answer][' + num + ']" class="regular-text" /></label></div>'
-      );
-        
-    //add another answer field to the preview
-    jQuery('.poll-list').append('<li class="poll-answer"><label><input type="radio" name="poll-preview" /></label></li>');
-    
-    //lastly add an event handler on change for preview purposes
-    jQuery('input[name="evalu_form[poll][answer][' + num + ']"]').on('change', function(element) {
-      Evaluate_Admin.previewPoll(element);
-    })
-  },
-
-  /*
-   *removes the last inserted question answer field from the metric form
-   *makes sure at least 2 answers remain at all times
-   */
-  removeLastAnswer : function() {
-    var answers = jQuery('input[name*="evalu_form[poll][answer]"]');
-    var num = answers.length;
-    if(num < 3) {
-    //do nothing, maybe display a warning?
-    } else {
-      var removed = answers.splice(-1,1);
-      var index = jQuery(removed).parent().parent().index() - 2;
-      var answer = jQuery('.poll-answer').get(index);
-      jQuery(answer).remove(); //remove answer from preview
-      jQuery(removed).parent().parent().remove(); //remove the parent-parent (grandparent? lol) of the input field
-    }
-  },
-
-  /*
-   *function to run after the DOM loads in the metric form
-   */
-  addFormReady : function() {
-    //remove style selection details at the start
-    Evaluate_Admin.showTypeSub();
-    //event handler for type selection
-    jQuery('input[name="evalu_form[type]"]').on('focus click', function(event) {
-      if(event.type == 'focus' || event.type == 'click') {
-        //"this" refers to the DOM element itself
-        Evaluate_Admin.type = jQuery(this).val();
-        Evaluate_Admin.style = ''; //reset style in case old entry exists
-        Evaluate_Admin.showTypeSub(this);
-        Evaluate_Admin.refreshPreview();
-      }
-    });
-    
-    //trigger type selection event if something is already selected upon page load
-    jQuery('.type_label input:checked').trigger('focus');
+	////the two global variables, required to display
+	//a preview of the metric before form submission
+	type: new String,
+	style: new String,
   
-    //event handler for style selection
-    jQuery('input[name="evalu_form[style]"]').on('focus click', function(event) {
-      if(event.type == 'focus' || event.type == 'click') {
-        Evaluate_Admin.style = jQuery(this).val();
-        Evaluate_Admin.refreshPreview();
-      }
-    });
-    
-    //trigger style selection event if there is preexisting data
-    jQuery('input[name="evalu_form[style]"]:checked').trigger('focus');
+	/*
+	 * Adds a new poll question answer in the metric form
+	 */
+	addNewAnswer: function() {
+		var answers = jQuery('input[name*="evalu_form[poll][answer]"]'); //fetch all active answer inputs
+		var num = answers.length + 1; //determine answer number
+		var last_field = answers.splice( -1, 1 ); // get last element
+		
+		//insert new answer field after the current last one
+		//since we select by input name, the parent of parent i.e. <div class="indent"> will be the element
+		//which we add the last field after.
+		jQuery(last_field).parent().parent().after(
+			'<div class="indent"><label>Answer '+num+': <input type="text" name="evalu_form[poll][answer]['+num+']" class="regular-text" /></label></div>'
+		);
+		
+		//add another answer field to the preview
+		jQuery('.poll-list').append('<li class="poll-answer"><label><input type="radio" name="poll-preview" /></label></li>');
+		
+		//lastly add an event handler on change for preview purposes
+		jQuery('input[name="evalu_form[poll][answer]['+num+']"]').on('change', function(element) {
+			Evaluate_Admin.previewPoll(element);
+		})
+	},
   
-    //event handler for display_name text change
-    jQuery('input[name="evalu_form[name]"]').on('change', Evaluate_Admin.refreshPreview);
-    jQuery('input[name="evalu_form[display_name]"]').on('focus click', Evaluate_Admin.refreshPreview);
-    
-    //event handler for poll preview
-    jQuery('input[name*="evalu_form[poll]"]').on('change', function(element) { 
-      Evaluate_Admin.previewPoll(element);
-    });
-    
-    //trigger change in case the poll data remains from a previous attempt
-    jQuery('input[name*="evalu_form[poll]"]').trigger('change');
-    
-    //finally refresh poll preview if possible
-    Evaluate_Admin.refreshPoll();
-  },
-
-  /*
-   *shows/hides further options in the metric form when specific parts are toggled
-   */
-  showTypeSub : function(element) {
-    var optionsList = jQuery('.type_options > li'); //all metric type menus
-    var section = jQuery(element).parent().parent(); //selected section
-    jQuery(optionsList).children().not('.type_label').hide(); //hide everything
-    //jQuery('input:hidden"').removeAttr('checked'); //deselect hidden style choice
-    jQuery(section).children().show(); //show selected section
-  },
-
-  /*
-   *refreshes the compoenents required to display a preview
-   *before submitting the form
-   */
-  refreshPreview : function(element) {
-    jQuery('div[id*="prev_"]').hide();
-    if(Evaluate_Admin.type != '') { //prevent premature refresh if no type is selected
-      jQuery('div[id*="prev_'+Evaluate_Admin.type+'_'+Evaluate_Admin.style+'"]').show();
-    }
-    jQuery('#preview_name').html(jQuery('input[name="evalu_form[name]"]').val());
-    jQuery('#preview_name').toggle(jQuery('input[name="evalu_form[display_name]"]').is(':checked'));
-  },
+	/*
+	 * Removes the last inserted question answer field from the metric form
+	 * makes sure at least 2 answers remain at all times
+	 */
+	removeLastAnswer: function() {
+		var answers = jQuery('input[name*="evalu_form[poll][answer]"]');
+		var num = answers.length;
+		
+		if (num >= 3) {
+			var removed = answers.splice( -1, 1 );
+			var index = jQuery(removed).parent().parent().index() - 2;
+			var answer = jQuery('.poll-answer').get(index);
+			jQuery(answer).remove(); //remove answer from preview
+			jQuery(removed).parent().parent().remove(); //remove the parent-parent (grandparent? lol) of the input field
+		} else {
+			//do nothing, maybe display a warning?
+		}
+	},
   
-  /*
-   *updates the poll fields in the preview as the real fields get updated
-   */
-  previewPoll : function(element) {
-    var element = jQuery(element.target);
-    if(element.attr('name') == 'evalu_form[poll][question]') {
-      jQuery('.poll-question').html(element.val());
-    } else {
-      /* the index is actually -2, because jQuery uses 0-indexes, and the answers are within individual divs,
-       * with a leading element for the question */
-      var index = element.parent().parent().index() - 2;
-      var answer = jQuery('.poll-answer').get(index);
-      jQuery(answer).html('<label><input type="radio" name="poll-preview" />' + element.val() + '</label>');
-    }
-  },
+	/*
+	 * Function to run after the DOM loads in the metric form
+	 */
+	addFormReady: function() {
+		//remove style selection details at the start
+		Evaluate_Admin.showTypeSub();
+		
+		//event handler for type selection
+		jQuery('input[name="evalu_form[type]"]').on( 'focus click', function(event) {
+			if ( event.type == 'focus' || event.type == 'click' ) {
+				//"this" refers to the DOM element itself
+				Evaluate_Admin.type = jQuery(this).val();
+				Evaluate_Admin.style = ''; //reset style in case old entry exists
+				Evaluate_Admin.showTypeSub(this);
+				Evaluate_Admin.refreshPreview();
+			}
+		} );
+		
+		//trigger type selection event if something is already selected upon page load
+		jQuery('.type_label input:checked').trigger('focus');
+	  
+		//event handler for style selection
+		jQuery('input[name="evalu_form[style]"]').on('focus click', function(event) {
+			if ( event.type == 'focus' || event.type == 'click' ) {
+				Evaluate_Admin.style = jQuery(this).val();
+				Evaluate_Admin.refreshPreview();
+			}
+		});
+		
+		//trigger style selection event if there is preexisting data
+		jQuery('input[name="evalu_form[style]"]:checked').trigger('focus');
+	  
+		//event handler for display_name text change
+		jQuery('input[name="evalu_form[name]"]').on( 'change', Evaluate_Admin.refreshPreview );
+		jQuery('input[name="evalu_form[display_name]"]').on( 'focus click', Evaluate_Admin.refreshPreview );
+		
+		//event handler for poll preview
+		jQuery('input[name*="evalu_form[poll]"]').on( 'change', function(element) { 
+			Evaluate_Admin.previewPoll(element);
+		} );
+		
+		//trigger change in case the poll data remains from a previous attempt
+		jQuery('input[name*="evalu_form[poll]"]').trigger('change');
+		
+		//finally refresh poll preview if possible
+		Evaluate_Admin.refreshPoll();
+	},
   
-  /*
-   *refreshes all elements in the poll preview display to sync with input fields
-   *should only be needed when user wants to edit metric, upon first load to load poll answers correctly
-   */
-  refreshPoll : function() {
-    jQuery('.poll-question').html(jQuery('input[name="evalu_form[poll][question]"]').val()); //sync question
-    var answer_fields = jQuery('input[name*="evalu_form[poll][answer]"]');
-    for(var i=1; i<=answer_fields.length; i++) { //loop through text answer fields and change answers
-      if(jQuery('.poll-answer').get(i-1)) { //-1 because inputs start from 1, index starts from 0
-      } else {
-        //add another answer field to the preview
-        jQuery('.poll-list').append('<li class="poll-answer"><label><input type="radio" name="poll-preview" /></label></li>');
-        jQuery('input[name*="evalu_form[poll][answer]['+i+']"]').trigger('change'); //trigger so previewPoll runs to change the preview to match the field
-      }
-    }
-  }
+	/*
+	 * Shows/hides further options in the metric form when specific parts are toggled
+	 */
+	showTypeSub: function( element ) {
+		var optionsList = jQuery('.type_options > li'); //all metric type menus
+		var section = jQuery(element).parent().parent(); //selected section
+		jQuery(optionsList).children().not('.type_label').hide(); //hide everything
+		//jQuery('input:hidden"').removeAttr('checked'); //deselect hidden style choice
+		jQuery(section).children().show(); //show selected section
+	},
+  
+	/*
+	 * Refreshes the compoenents required to display a preview
+	 * before submitting the form
+	 */
+	refreshPreview: function( element ) {
+		jQuery('div[id*="prev_"]').hide();
+		if ( Evaluate_Admin.type != '' ) { //prevent premature refresh if no type is selected
+			jQuery('div[id*="prev_'+Evaluate_Admin.type+'_'+Evaluate_Admin.style+'"]').show();
+		}
+		
+		jQuery('#preview_name').html( jQuery('input[name="evalu_form[name]"]').val() );
+		jQuery('#preview_name').toggle( jQuery('input[name="evalu_form[display_name]"]').is(':checked') );
+	},
+	
+	/*
+	 * Updates the poll fields in the preview as the real fields get updated
+	 */
+	previewPoll: function( element ) {
+		element = jQuery(element.target);
+		
+		if ( element.attr('name') == 'evalu_form[poll][question]' ) {
+			jQuery('.poll-question').html(element.val());
+		} else {
+			// The index is actually -2, because jQuery uses 0-indexes, and the answers are within individual divs,
+			// with a leading element for the question
+			var index = element.parent().parent().index() - 2;
+			var answer = jQuery('.poll-answer').get(index);
+			jQuery(answer).html('<label><input type="radio" name="poll-preview" />'+element.val()+'</label>');
+		}
+	},
+	
+	/*
+	 * Refreshes all elements in the poll preview display to sync with input fields
+	 * should only be needed when user wants to edit metric, upon first load to load poll answers correctly
+	 */
+	refreshPoll: function() {
+		jQuery('.poll-question').html( jQuery('input[name="evalu_form[poll][question]"]').val() ); //sync question
+		var answer_fields = jQuery('input[name*="evalu_form[poll][answer]"]');
+		
+		for( var i = 1; i <= answer_fields.length; i++ ) { //loop through text answer fields and change answers
+			if ( jQuery('.poll-answer').get(i-1) ) { //-1 because inputs start from 1, index starts from 0
+				// Do nothing
+			} else {
+				// Add another answer field to the preview
+				jQuery('.poll-list').append('<li class="poll-answer"><label><input type="radio" name="poll-preview" /></label></li>');
+				jQuery('input[name*="evalu_form[poll][answer]['+i+']"]').trigger('change'); // Trigger so previewPoll runs to change the preview to match the field
+			}
+		}
+	}
 };
 
 /*
@@ -155,16 +162,18 @@ var Evaluate_Admin = {
  * jQuery(document).ready() does NOT work here
  */
 jQuery(window).load(function() {  
-  if(jQuery('input[name="evalu_form[type]"]').length) { //length > 0 i.e element exists
-    Evaluate_Admin.addFormReady();
-  }
-  //disable metric links if on admin panel
-  links = jQuery('.evaluate-shell a');
-  forms = jQuery('.evaluate-shell form');
-  if(links.length > 0) {
-    jQuery.each(links, function(index, value){jQuery(value).attr('href', 'javascript:void(0)')});
-  }
-  if(forms.length > 0) {
-    jQuery.each(forms, function(index, value){jQuery(value).live('submit', function(event){event.preventDefault();})});
-  }
+	if ( jQuery('input[name="evalu_form[type]"]').length ) { //length > 0 i.e element exists
+		Evaluate_Admin.addFormReady();
+	}
+	
+	//disable metric links if on admin panel
+	links = jQuery('.evaluate-shell a');
+	if ( links.length > 0 ) {
+		jQuery.each(links, function(index, value){jQuery(value).attr('href', 'javascript:void(0)')});
+	}
+	
+	forms = jQuery('.evaluate-shell form');
+	if ( forms.length > 0 ) {
+		jQuery.each(forms, function(index, value){jQuery(value).live('submit', function(event){event.preventDefault();})});
+	}
 });
--- js/evaluate.js
+++ js/evaluate.js
@@ -1,160 +1,175 @@
 var Evaluate = {
-  attach: function() {
-    if(evaluate_ajax.use_ajax) {
-      jQuery('.eval-link').live('click', Evaluate.attachToLink);
-      jQuery('form[name="poll-form"]').live('submit', Evaluate.attachToPoll);
-      jQuery('.poll-div a').live('click', Evaluate.attachToPollLink);
-    }
-  },
-  
-  attachToLink: function(event) {
-    if(!evaluate_ajax.use_ajax && !evaluate_ajax.stream_active) {
-      return; // use url directly
-    }
-    event.preventDefault();
-    var args = Evaluate.parseUrl(this.href);
-    var element = this;
-    args['_wpnonce'] = jQuery(this).data('nonce');
-    jQuery.post(evaluate_ajax.ajaxurl, {
-      action: 'evaluate-vote',
-      data: args
-    }, function(data) {
-      if(evaluate_ajax.use_ajax && !evaluate_ajax.stream_active) {
-	jQuery(element).closest('.evaluate-shell').replaceWith(data);
-      } //else client will receive the update from socketio
-    });
-  },
-  
-  parseUrl: function(string) {
-    var vars = {};
-    var parts = string.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
-      vars[key] = value;
-    });
-    return vars;
-  },
-  
-  attachToPoll: function(event) {
-    if(!evaluate_ajax.use_ajax && !evaluate_ajax.stream_active) {
-      return; // use url directly
-    }
-    event.preventDefault();
-    var element = this;
-    var args = Evaluate.parseUrl('?' + jQuery(element).serialize());
-    if(typeof args['vote'] == 'undefined') return;
-    jQuery.post(evaluate_ajax.ajaxurl, {
-      action: 'evaluate-vote',
-      data: args
-    }, function(data) {
-      if(evaluate_ajax.use_ajax && !evaluate_ajax.stream_active) {
-	jQuery(element).closest('.evaluate-shell').replaceWith(data);
-      }
-    });
-  },
-  
-  attachToPollLink: function(event) {
-    event.preventDefault();
-    var element = this;
-    var args = Evaluate.parseUrl(this.href);
-    jQuery.post(evaluate_ajax.ajaxurl, {
-      action: 'evaluate-vote',
-      data: args
-    }, function(data) {
-      jQuery(element).closest('div[id^=evaluate-shell-]').html(data);
-    });
-  }
+    attach: function() {
+        if ( evaluate_ajax.use_ajax ) {
+            jQuery('.eval-link').live( 'click', Evaluate.attachToLink );
+            jQuery('form[name="poll-form"]').live( 'submit', Evaluate.attachToPoll );
+            jQuery('.poll-div a').live( 'click', Evaluate.attachToPollLink );
+        }
+    },
+    
+    attachToLink: function(event) {
+        if ( ! evaluate_ajax.use_ajax && ! evaluate_ajax.stream_active ) {
+            return; // use url directly
+        }
+        
+        event.preventDefault();
+        var args = Evaluate.parseUrl(this.href);
+        var element = this;
+        args['_wpnonce'] = jQuery(this).data('nonce');
+        
+        var data = {
+            action: 'evaluate-vote',
+            data: args
+        }
+        
+        jQuery.post( evaluate_ajax.ajaxurl, data, function( response ) {
+            if ( evaluate_ajax.use_ajax && ! evaluate_ajax.stream_active ) {
+                jQuery(element).closest('.evaluate-shell').replaceWith(response);
+            } //else client will receive the update from socketio
+        } );
+    },
+    
+    parseUrl: function(string) {
+        var vars = {};
+        var parts = string.replace(/[?&]+([^=&]+)=([^&]*)/gi, function( m, key, value ) {
+            vars[key] = value;
+        });
+        
+        return vars;
+    },
+    
+    attachToPoll: function(event) {
+        if ( ! evaluate_ajax.use_ajax && ! evaluate_ajax.stream_active ) {
+            return; // use url directly
+        }
+        
+        event.preventDefault();
+        var element = this;
+        var args = Evaluate.parseUrl('?' + jQuery(element).serialize());
+        if ( typeof args['vote'] == 'undefined' ) return;
+        
+        var data = {
+            action: 'evaluate-vote',
+            data: args
+        }
+        
+        jQuery.post( evaluate_ajax.ajaxurl, data, function( response ) {
+            if ( evaluate_ajax.use_ajax && ! evaluate_ajax.stream_active ) {
+                jQuery(element).closest('.evaluate-shell').replaceWith(response);
+            }
+        } );
+    },
+    
+    attachToPollLink: function( event ) {
+        event.preventDefault();
+        var element = this;
+        var args = Evaluate.parseUrl(this.href);
+        
+        var data = {
+            action: 'evaluate-vote',
+            data: args
+        }
+        
+        jQuery.post( evaluate_ajax.ajaxurl, data, function( response ) {
+            jQuery(element).closest('div[id^=evaluate-shell-]').html(response);
+        } );
+    },
 };
-
+  
 var template = {
-  'one-way': doT.template(jQuery('#evaluate-one-way').text()),
-  'two-way': doT.template(jQuery('#evaluate-two-way').text()),
-  'range': doT.template(jQuery('#evaluate-range').text()),
-  'poll-form': doT.template(jQuery('#evaluate-poll-form').text()),
-  'poll-results': doT.template(jQuery('#evaluate-poll-results').text())
+    'one-way'     : doT.template(jQuery('#evaluate-one-way').text()),
+    'two-way'     : doT.template(jQuery('#evaluate-two-way').text()),
+    'range'       : doT.template(jQuery('#evaluate-range').text()),
+    'poll-form'   : doT.template(jQuery('#evaluate-poll-form').text()),
+    'poll-results': doT.template(jQuery('#evaluate-poll-results').text()),
 };
 
 /* page load */
 jQuery(window).load(function() {
-  Evaluate.attach();
-  
-  if(typeof CTLT_Stream != "undefined") {
-    CTLT_Stream.on('server-push', function (data) {
-      //if data received is relevant to evaluate
-      if( 'evaluate' == data.type ){
-	var element = jQuery('div[id^=evaluate-shell-'+data.data.metric_id+'-'+data.data.content_id+']'); //element to be changed
-	//check metric type and make the adjustments needed
-	switch(data.data.type) {
-	  case 'one-way':
-	    data.data.nonce = element.find('.eval-link').data('nonce'); //re-fill nonce field
-	    if( data.data.user == evaluate_ajax.user ) {
-	      element.data('user-vote', data.data.user_vote);
-	    } else {
-	      data.data.user_vote = element.data('user-vote');
-	      if(data.data.user_vote == 1) {
-		data.data.state = '-selected';
-	      } else {
-		data.data.state = '';
-	      }
-	    }
-	    element.html(template['one-way'](data.data));
-	    break;
-          
-	  case 'two-way':
-	    data.data.nonce_up = element.find('.link-up').data('nonce');
-	    data.data.nonce_down = element.find('.link-down').data('nonce');
-	    if( data.data.user == evaluate_ajax.user ) {
-	      element.data('user-vote', data.data.user_vote);
-	    } else {
-	      data.data.user_vote = element.data('user-vote');
-	      if(data.data.user_vote == 1) {
-		data.data.state_up = '-selected';
-		data.data.state_down = '';
-	      } else if(data.data.user_vote == -1) {
-		data.data.state_up = '';
-		data.data.state_down = '-selected';
-	      } else {
-		data.data.state_up = '';
-		data.data.state_down = '';
-	      }
-	    }
-	    element.html(template['two-way'](data.data));
-	    break;
-          
-	  case 'range':
-	    for(var i=1; i<=5; i++) {
-	      data.data.nonce[i] = element.find('.link-'+i).data('nonce');
-	    }
-	    if( data.data.user == evaluate_ajax.user ) {
-	      element.data('user-vote', data.data.user_vote);
-	    } else {
-	      data.data.user_vote = element.data('user-vote');
-	      if(data.data.user_vote) {
-		data.data.state = '-selected';
-		data.data.width = data.data.user_vote / 5.0 * 100;
-	      } else {
-		data.data.state = '';
-		data.data.width = data.data.average / 5.0 * 100;
-	      }
-	    }
-	    element.html(template['range'](data.data));
-	    break;
-          
-	  case 'poll':
-	    if( data.data.user == evaluate_ajax.user ) {
-	      element.data('user-vote', data.data.user_vote);
-	      element.html(template['poll-results'](data.data));
-	    } else {
-	      data.data._wpnonce = element.find('input[name="_wpnonce"]').val();
-	      data.data.user_vote = element.data('user-vote');
-	      if(element.find('.poll-results').length > 0) {
-		element.html(template['poll-results'](data.data));
-	      } else {
-		element.html(template['poll-form'](data.data));
-	      }
-	    }
-	    break;
-	}
-      }
-    });
-  }
-  
-});
+    Evaluate.attach();
+    
+    if ( typeof CTLT_Stream != "undefined" ) {
+        CTLT_Stream.on('server-push', function (data) {
+            //if data received is relevant to evaluate
+            if ( 'evaluate' == data.type ) {
+                var element = jQuery('div[id^=evaluate-shell-'+data.data.metric_id+'-'+data.data.content_id+']'); //element to be changed
+                
+                //check metric type and make the adjustments needed
+                switch ( data.data.type ) {
+                case 'one-way':
+                    data.data.nonce = element.find('.eval-link').data('nonce'); //re-fill nonce field
+                    if ( data.data.user == evaluate_ajax.user ) {
+                        element.data( 'user-vote', data.data.user_vote );
+                    } else {
+                        data.data.user_vote = element.data('user-vote');
+                        
+                        if ( data.data.user_vote == 1 ) {
+                            data.data.state = '-selected';
+                        } else {
+                            data.data.state = '';
+                        }
+                    }
+                    
+                    element.html(template['one-way'](data.data));
+                    break;
+                case 'two-way':
+                    data.data.nonce_up = element.find('.link-up').data('nonce');
+                    data.data.nonce_down = element.find('.link-down').data('nonce');
+                    
+                    if ( data.data.user == evaluate_ajax.user ) {
+                        element.data('user-vote', data.data.user_vote);
+                    } else {
+                        data.data.user_vote = element.data('user-vote');
+                        if ( data.data.user_vote == 1 ) {
+                            data.data.state_up = '-selected';
+                            data.data.state_down = '';
+                        } else if ( data.data.user_vote == -1 ) {
+                            data.data.state_up = '';
+                            data.data.state_down = '-selected';
+                        } else {
+                            data.data.state_up = '';
+                            data.data.state_down = '';
+                        }
+                    }
+                    
+                    element.html(template['two-way'](data.data));
+                    break;
+                case 'range':
+                    for ( var i = 1; i <= 5; i++ ) {
+                        data.data.nonce[i] = element.find('.link-'+i).data('nonce');
+                    }
+                    
+                    if ( data.data.user == evaluate_ajax.user ) {
+                        element.data('user-vote', data.data.user_vote);
+                    } else {
+                        data.data.user_vote = element.data('user-vote');
+                        if ( data.data.user_vote ) {
+                            data.data.state = '-selected';
+                            data.data.width = data.data.user_vote / 5.0 * 100;
+                        } else {
+                            data.data.state = '';
+                            data.data.width = data.data.average / 5.0 * 100;
+                        }
+                    }
+                    
+                    element.html(template['range'](data.data));
+                    break;
+                case 'poll':
+                    if ( data.data.user == evaluate_ajax.user ) {
+                        element.data('user-vote', data.data.user_vote);
+                        element.html(template['poll-results'](data.data));
+                    } else {
+                        data.data._wpnonce = element.find('input[name="_wpnonce"]').val();
+                        data.data.user_vote = element.data('user-vote');
+                        if ( element.find('.poll-results').length > 0 ) {
+                            element.html(template['poll-results'](data.data));
+                        } else {
+                            element.html(template['poll-form'](data.data));
+                        }
+                    }
+                    break;
+                }
+            }
+        } );
+    }
+} );
